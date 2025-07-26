24546ec779031fe7c29f7e15ca15b0d450e300c8
Maria <orriole@yandex.ru>
2025-07-26 15:33:50 +0300

    Implemented telegram command for displaying walks by day

--- app/src/HealthTracker/Application/Telegram/Query/Walk/FindWalksByDate/FindWalksByDateQueryHandler.php
+++ app/src/HealthTracker/Application/Telegram/Query/Walk/FindWalksByDate/FindWalksByDateQueryHandler.php
@@ -2,44 +2,42 @@
 
 declare(strict_types=1);
 
-namespace App\HealthTracker\Application\Telegram\Command\AddWalk;
+namespace App\HealthTracker\Application\Telegram\Query\Walk\FindWalksByDate;
 
 use App\HealthTracker\Application\DTO\WalkData;
+use App\HealthTracker\Domain\Entity\Walk;
 use App\HealthTracker\Domain\Exception\UserInfoNotFilledException;
-use App\HealthTracker\Domain\Factory\WalkFactoryInterface;
 use App\HealthTracker\Domain\Repository\UserRepositoryInterface;
 use App\HealthTracker\Domain\Repository\WalkRepositoryInterface;
 use App\HealthTracker\Domain\ValueObject\User\TelegramUserId;
-use App\Shared\Application\Command\CommandHandlerInterface;
+use App\Shared\Application\Query\QueryHandlerInterface;
 
-final readonly class AddWalkCommandHandler implements CommandHandlerInterface
+final readonly class FindWalksByDateQueryHandler implements QueryHandlerInterface
 {
     public function __construct(
         private UserRepositoryInterface $userRepository,
         private WalkRepositoryInterface $walkRepository,
-        private WalkFactoryInterface $walkFactory,
     ) {}
 
-    public function __invoke(AddWalkCommand $command): AddWalkCommandResult
+    public function __invoke(FindWalksByDateQuery $query): FindWalksByDateQueryResult
     {
         $user = $this->userRepository->findByTelegramUserIdOrFail(
-            new TelegramUserId($command->telegramUserId)
+            new TelegramUserId($query->telegramUserId)
         );
 
         if (!$user->isFilled()) {
             throw new UserInfoNotFilledException('Не заполнены данные пользователя');
         }
 
-        $walk = $this->walkFactory->create(
-            user: $user,
-            steps: $command->steps
-        );
+        $walksByDate = $this->walkRepository->findWalksByDate($user, $query->date);
 
-        $this->walkRepository->save($walk);
-
-        return new AddWalkCommandResult(
-            walk: WalkData::fromEntity($walk),
-            daySteps: $this->walkRepository->getTotalStepsToday($user),
+        return new FindWalksByDateQueryResult(
+            date: $query->date,
+            walks: array_map(
+                fn(Walk $walk): WalkData => WalkData::fromEntity($walk),
+                $walksByDate
+            ),
+            daySteps: $this->walkRepository->getTotalStepsByDate($user, $query->date),
             dailyNormSteps: $user->dailyNorm->steps->value(),
         );
     }
---
+++ app/src/HealthTracker/Infrastructure/Telegram/Command/WalksByDayTelegramCommand.php
@@ -0,0 +1,192 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\HealthTracker\Infrastructure\Telegram\Command;
+
+use App\HealthTracker\Application\Telegram\Query\Walk\FindWalksByDate\FindWalksByDateQuery;
+use App\HealthTracker\Application\Telegram\Query\Walk\FindWalksByDate\FindWalksByDateQueryResult;
+use App\HealthTracker\Application\Telegram\Query\Walk\GetDateWithWalks\GetDateWithWalksQuery;
+use App\HealthTracker\Domain\Enum\Direction;
+use App\HealthTracker\Infrastructure\Telegram\Enum\TelegramCommand;
+use App\HealthTracker\Infrastructure\Telegram\Message\MessagePayload;
+use App\Shared\Application\Bus\QueryBusInterface;
+use DateMalformedStringException;
+use DateTimeImmutable;
+use DateTimeInterface;
+use TelegramBot\Api\BotApi;
+use TelegramBot\Api\Exception;
+use TelegramBot\Api\InvalidArgumentException;
+use TelegramBot\Api\Types\Inline\InlineKeyboardMarkup;
+use TelegramBot\Api\Types\Update;
+use Twig\Environment;
+
+final class WalksByDayTelegramCommand extends BaseTelegramCommand
+{
+    public function __construct(
+        Environment $twig,
+        QueryBusInterface $queryBus,
+    )
+    {
+        parent::__construct($twig, $queryBus);
+    }
+
+    public function getName(): string
+    {
+        return TelegramCommand::WALKS_BY_DAY->value;
+    }
+
+    public function getAliases(): array
+    {
+        return [
+            TelegramCommand::WALKS_BY_DAY->getAlias(),
+        ];
+    }
+
+    public function getDescription(): string
+    {
+        return 'Показывает историю прогулок за день';
+    }
+
+    public function getSortOrder(): int
+    {
+        return 600;
+    }
+
+    /**
+     * @param BotApi $api
+     * @param Update $update
+     * @return void
+     * @throws Exception
+     * @throws InvalidArgumentException
+     * @throws DateMalformedStringException
+     */
+    public function execute(BotApi $api, Update $update): void
+    {
+        parent::execute($api, $update);
+
+        $message = $this->telegramMessage;
+        $chatId = (string)$message?->getChat()->getId();
+
+        $date = $this->getDate($update);
+        $date = $date ?: new DateTimeImmutable();
+
+        $messageId = $update->getCallbackQuery()?->getMessage()->getMessageId();
+
+        /** @var FindWalksByDateQueryResult $result */
+        $result = $this->queryBus->ask(
+            new FindWalksByDateQuery(
+                telegramUserId: $this->telegramUser->getId(),
+                date: $date,
+            ),
+        );
+
+        $text = $this->renderTemplate($this->getSuccessMessageTemplate(), $result->toArray());
+        $buttons = $this->getButtons($date);
+        $inlineKeyboard = new InlineKeyboardMarkup($buttons);
+
+        if ($messageId) {
+            $api->editMessageText(
+                $chatId,
+                $messageId,
+                $text,
+                'html',
+                true,
+                $inlineKeyboard
+            );
+        } else {
+            $payload = new MessagePayload(
+                chatId: $chatId,
+                text: $text,
+                replyMarkup: $inlineKeyboard,
+            );
+
+            $this->sendApiMessage($api, $payload);
+        }
+    }
+
+    /**
+     * @param Update $update
+     * @return bool
+     * @throws DateMalformedStringException
+     */
+    public function isApplicable(Update $update): bool
+    {
+        if (parent::isApplicable($update)) {
+            return true;
+        }
+
+        return $this->getDate($update) !== null;
+    }
+
+    /**
+     * @param Update $update
+     * @return DateTimeInterface|null
+     * @throws DateMalformedStringException
+     */
+    private function getDate(Update $update): ?DateTimeInterface
+    {
+        $regexp = '#' . TelegramCommand::WALKS_BY_DAY->value . '_(\d{4}-\d{2}-\d{2})#';
+
+        if ($update->getMessage() && preg_match($regexp, $update->getMessage()->getText(), $matches)) {
+            return new DateTimeImmutable($matches[1]);
+        }
+
+        if ($update->getCallbackQuery() && preg_match($regexp, $update->getCallbackQuery()->getData(), $matches)) {
+            return new DateTimeImmutable($matches[1]);
+        }
+
+        return null;
+    }
+
+    protected function getSuccessMessageTemplate(): string
+    {
+        return 'health-tracker/telegram/command/walks-by-day.html.twig';
+    }
+
+    private function getButtons(DateTimeInterface $date): array
+    {
+        $buttons = [];
+
+        $prevDateWithWalks = $this->getPrevDateWithWalks($date);
+        $nextDateWithWalks = $this->getNextDateWithWalks($date);
+
+        if ($prevDateWithWalks !== null) {
+            $buttons[] = [
+                'text' => '<< ' . $prevDateWithWalks->format('d.m.Y'),
+                'callback_data' => TelegramCommand::WALKS_BY_DAY->value . '_' . $prevDateWithWalks->format('Y-m-d'),
+            ];
+        }
+
+        if ($nextDateWithWalks !== null) {
+            $buttons[] = [
+                'text' => $nextDateWithWalks->format('d.m.Y') . ' >>',
+                'callback_data' => TelegramCommand::WALKS_BY_DAY->value . '_' . $nextDateWithWalks->format('Y-m-d'),
+            ];
+        }
+
+        return [$buttons];
+    }
+
+    private function getPrevDateWithWalks(DateTimeInterface $date): ?DateTimeInterface
+    {
+        return $this->queryBus->ask(
+            new GetDateWithWalksQuery(
+                telegramUserId: $this->telegramUser->getId(),
+                date: $date,
+                direction: Direction::PREV
+            )
+        );
+    }
+
+    private function getNextDateWithWalks(DateTimeInterface $date): ?DateTimeInterface
+    {
+        return $this->queryBus->ask(
+            new GetDateWithWalksQuery(
+                telegramUserId: $this->telegramUser->getId(),
+                date: $date,
+                direction: Direction::NEXT
+            )
+        );
+    }
+}
